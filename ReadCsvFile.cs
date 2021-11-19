fb221072278549e145158f1b2c46d3f3473a5080
陳沂 <polochen@ytsys.com>
2021-11-19 15:16:27 +0800

    將"以 IF 值升冪排序"修正為降冪排序

--- ReadCsvFile.cs
+++ ReadCsvFile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -25,6 +26,80 @@ namespace WoSDataConvertor
                 Console.WriteLine(ex.Message);
                 return null;
             }
+        }
+
+        /// <summary>
+        /// 將修改前的 CSV 經過處理後，存放到 DataTable 與 DataSet
+        /// </summary>
+        /// <param name="fileInfo"></param>
+        public static DataSet CsvToDataTable(FileInfo[] fileInfo)
+        {
+            DataSet Ds = new DataSet();
+            DataTable Dt = new DataTable();
+            int RowCount;
+            string Category;
+            string JournalRank;
+            string TotalJournal;
+            string ImpactFactor;
+            string Quartile;
+            try
+            {
+                for (int i = 0; i < fileInfo.Length; i++)       // 走訪修改前資料夾下的所有檔案
+                {
+                    Dt = GetFileDataTables.GetBeforeModifyDt();
+                    RowCount = 1;
+                    var reader = new StreamReader(File.OpenRead(fileInfo[i].FullName));
+                    while (!reader.EndOfStream)
+                    {
+                        var DataLine = reader.ReadLine().Replace(", ", "，");       //讀取資料列，並先取代 "逗號+空白" 避免因 Category 的名稱導致資料切分錯誤
+                        var Values = DataLine.Split(',');   //用指定符號切割資料
+                        if (RowCount < 4 && Values.Length > 0)     // 避免讀取到格式不正確的資料列
+                        {
+                            RowCount++;
+                        }
+                        else if (RowCount >= 4 && Values.Length > 5)      // 讀取正確資料
+                        {
+                            for (int j = 0; j < Values[3].Split(';').Length; j++)
+                            {
+                                Category = Values[3];
+                                // IF值為 n/a，則領域期刊數、期刊排名、分位數為 n/a
+                                if (Values[5].Contains("n/a"))
+                                {
+                                    JournalRank = "n/a";
+                                    TotalJournal = "n/a";
+                                    Quartile = "n/a";
+                                    ImpactFactor = "n/a";
+                                }
+                                else
+                                {
+                                    JournalRank = "0";
+                                    TotalJournal = "";
+                                    Quartile = Values[4].Replace("\"", "");
+                                    ImpactFactor = Values[5].Replace("\"", "");
+                                }
+                                Category = Category.Replace("; ", ";").Replace("，", ", ");     //將全形逗號還原回來
+                                Dt.Rows.Add(Values[0].Replace("\"", ""),
+                                    Category.Split(';')[j].Replace("\"", ""),
+                                    Quartile,
+                                    ImpactFactor,
+                                    JournalRank,
+                                    TotalJournal);
+                            }
+                        }
+                    }
+                    Dt = Dt.AsEnumerable()      // 以 IF 值降冪排序
+                        .OrderByDescending(r => r.Field<string>("JIF"))
+                        .CopyToDataTable();
+                    Dt.TableName = fileInfo[i].Name.Replace(".csv", "");
+                    Ds.Tables.Add(Dt);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("讀取 csv 發生錯誤");
+            }
+
+            return Ds;
         }
     }
 }
